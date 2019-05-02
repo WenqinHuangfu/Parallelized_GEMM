ab88beb3e14291d575c92af9ed41f4a7942cc06b
Wenqin Huangfu <wenqinhf@gmail.com>
2019-05-01 19:13:10 -0700

    Update dgemm-openmp.cpp

--- dgemm-openmp.cpp
+++ dgemm-openmp.cpp
@@ -4,12 +4,8 @@
 const char *dgemm_desc = "openMP, three-loop dgemm.";
 void square_dgemm( int n, double *A, double *B, double *C)
 {
-  //omp_set_num_threads(thread_count);
-  printf("enter matrix multiplication using openmp\n");
-  printf("start computation, num_threads=%i\n",omp_get_num_threads());
   #pragma omp parallel
   {
-  printf("thread_num=%i\n",omp_get_thread_num());
   int i,j,k;
   #pragma omp for
   for( i = 0; i < n; i++ )
